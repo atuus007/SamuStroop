6353d0e3dae35a786316b36fbf785134399a1b26
Norbert Bátfai <batfai.norbert@inf.unideb.hu>
2016-01-16 11:37:56 +0100

    Update doc

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SamuStroop
-Samu (Hanshon) experiencing with the Stroop effect
+[Samu](http://arxiv.org/abs/1511.02889) (Hanshon) experiencing with the Stroop effect
+This is an example of the manuscript entitled "Samu in his prenatal development".
+
+This project uses the same COP-based Q-learning engine as Samu ([Nahshon](https://github.com/nbatfai/nahshon)) 
+to predict the next sentences of a conversation. 
+
+After the basic work on the joining 
+of Conway's Game of Life and Samu's COP engine is [done](https://github.com/nbatfai/SamuLife), 
+I had started to work on creating a similar but much simpler example 
+in which Samu must learn the frames of the [moving picture](https://github.com/nbatfai/SamuMovie).
+
+The experiments started in this project are based on an even simpler model in which Samu must learn stills.
+
+## Usage
+
+```
+git clone https://github.com/nbatfai/SamuStroop.git
+cd SamuStroop/
+~/Qt/5.5/gcc_64/bin/qmake SamuLife.pro
+make
+./SamuStroop
+```
+[https://youtu.be/VujHHeYuzIk](https://youtu.be/VujHHeYuzIk)
+
+![samustroop](https://cloud.githubusercontent.com/assets/3148120/12371932/893eca66-bc45-11e5-9b29-aa19884a551c.png)
---
+++ SamuLife.cpp
@@ -0,0 +1,139 @@
+/**
+ * @brief Samu has learnt the rules of Conway's Game of Life
+ *
+ * @file GameOfLife.h
+ * @author  Norbert Bátfai <nbatfai@gmail.com>
+ * @version 0.0.1
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2015, 2016 Norbert Bátfai, batfai.norbert@inf.unideb.hu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * Samu (Nahshon) has learnt the rules of Conway's Game of Life. This is
+ * an example of the paper entitled "Samu in his prenatal development".
+ */
+
+#include "SamuLife.h"
+
+SamuLife::SamuLife ( int w, int h, QWidget *parent ) : QMainWindow ( parent )
+{
+  setWindowTitle ( "SamuStroop" );
+  setFixedSize ( QSize ( 2*w*m_cw, h*m_ch ) );
+
+  gameOfLife = new GameOfLife ( w, h );
+  gameOfLife->start();
+
+  connect ( gameOfLife, SIGNAL ( cellsChanged ( int **, int ** ) ),
+            this, SLOT ( updateCells ( int **, int ** ) ) );
+
+}
+
+void SamuLife::updateCells ( int **lattice, int **prediction )
+{
+  this->lattice = lattice;
+  this->prediction = prediction;
+  update();
+}
+
+void SamuLife::paintEvent ( QPaintEvent* )
+{
+  QPainter qpainter ( this );
+
+
+
+  for ( int i {0}; i<gameOfLife->getH(); ++i )
+    {
+      for ( int j {0}; j<gameOfLife->getW(); ++j )
+        {
+
+          if ( lattice )
+            {
+              if ( lattice[i][j] == 1 )
+                qpainter.fillRect ( j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::red );
+              else if ( lattice[i][j] == 2 )
+                qpainter.fillRect ( j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::green );
+              else if ( lattice[i][j] == 3 )
+                qpainter.fillRect ( j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::blue );
+              else
+                qpainter.fillRect ( j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::white );
+            }
+          if ( prediction )
+            {
+              if ( prediction[i][j] == 1 )
+                qpainter.fillRect ( gameOfLife->getW() *m_cw + j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::red );
+              else if ( prediction[i][j] == 2 )
+                qpainter.fillRect ( gameOfLife->getW() *m_cw + j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::green );
+              else if ( prediction[i][j] == 3 )
+                qpainter.fillRect ( gameOfLife->getW() *m_cw + j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::blue );
+              else
+                qpainter.fillRect ( gameOfLife->getW() *m_cw + j*m_cw, i*m_ch,
+                                    m_cw, m_ch, Qt::white );
+            }
+          qpainter.setPen ( QPen ( Qt::red, 1 ) );
+
+          qpainter.drawRect ( j*m_cw, i*m_ch,
+                              m_cw, m_ch );
+
+          qpainter.setPen ( QPen ( Qt::blue, 1 ) );
+
+          qpainter.drawRect ( gameOfLife->getW() *m_cw + j*m_cw, i*m_ch,
+                              m_cw, m_ch );
+        }
+    }
+
+  QFont font = qpainter.font() ;
+  font.setPointSize ( 28 );
+  qpainter.setFont ( font );
+  qpainter.setPen ( QPen ( Qt::red, 1 ) );
+  qpainter.drawText ( 40, 60, "Reality" );
+  qpainter.setPen ( QPen ( Qt::blue, 1 ) );
+  qpainter.drawText ( gameOfLife->getW() *m_cw +40, 60, "Samu's prediction" );
+  qpainter.setPen ( QPen ( Qt::gray, 1 ) );
+  qpainter.drawText ( 40, gameOfLife->getH() *m_ch - 30 , QString::number(gameOfLife->getT()) );
+
+  qpainter.end();
+}
+
+void SamuLife::keyPressEvent ( QKeyEvent * event )
+{
+  if ( event->key() == Qt::Key_Space )
+    {
+      gameOfLife->pause();
+    }
+  else if ( event->key() == Qt::Key_D) //Qt::Key_division )
+    {
+      gameOfLife->setDelay(gameOfLife->getDelay() / 2.0);
+    }
+  else if ( event->key() == Qt::Key_M) //Qt::Key_multiply )
+    {
+      gameOfLife->setDelay(gameOfLife->getDelay() * 2.0);
+    }
+}
+
+SamuLife::~SamuLife()
+{
+  delete gameOfLife;
+}
+
